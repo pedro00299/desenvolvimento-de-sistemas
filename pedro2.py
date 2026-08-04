5aea6eda012c9b489e840721126dc11d8e5fca8a
pedro00299 <ph0355652@gmail.com>
2026-08-04 13:02:46 -0300

    Add files via upload

---
+++ pedro2.py
@@ -0,0 +1,8 @@
+preco = 1000
+desconto = 50
+
+valordesconto = preco * desconto/100
+precofinal = preco - valordesconto
+
+print("valor do desconto: ", valordesconto)
+print("valor final: ", precofinal)
